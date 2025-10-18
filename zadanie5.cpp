4ae7961701135fb681f01164f7144fd0f4ef672e
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-18 13:44:19 +0200

    Add program to calculate travel cost

---
+++ zadanie5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	double koszt, cbenz, spal, odleg;
+	
+	cbenz = 5;
+		spal = 9;
+		odleg = 335;
+		koszt = cbenz * spal * odleg / 100;
+		cout << "koszt przejazdu"<< koszt;
+
+
+
+
+	
+
+	return 0;
