d1ed9d0665e11f95622a613c5c704d605def13b7
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-18 12:37:40 +0200

    Implement triangle area calculation in zadanie2.cpp

---
+++ zadanie2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	double a, h, pole;
+		cout << "podaj a:" << endl;
+	cin >> a;
+	cout << "podaj h:" << endl;
+	cin >> h;
+	pole = (a * h) / 2;
+	cout << "pole:" << pole;
+
+	return 0;
+}
