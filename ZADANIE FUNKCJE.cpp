b84004b68bc1a342bdf2cabb56ba398e61ba5cca
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-25 14:17:30 +0200

    Create ZADANIE FUNKCJE.cpp

---
+++ ZADANIE FUNKCJE.cpp
@@ -0,0 +1,15 @@
+#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+
+using namespace std;
+
+void powitanie() {
+	cout << "Witaj w swiecie c++!!!";
+}
+int main() {
+powitanie();
+return 0;
+
+}
