d3411329c87ceb95608376f91ad66517abc424af
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-25 10:21:25 +0200

    Implement multiplication table for user input
    Fix the multiplication table output and initialize loop variable.

---
+++ zadanie 12.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+    using namespace std;
+
+    int main() {
+        int n;
+        cout << "podaj liczbe" << endl;
+        cin >> n;
+        for (int i; i < 10; i++) {
+            cout << n << " * " << i << "*" << n * i << endl;
+        }
+        return 0
+    }
+
+
