dcd3ffdbd664e221818b1b8ffea8372c9b833546
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-25 09:54:11 +0200

    Create zadanie 11.cpp

---
+++ zadanie 11.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+    using namespace std;
+
+    int main() {
+        int n;
+        cout << "podaj liczbe" << endl;
+        cin >> n;
+        cout << n << "*1=" << n * 1 << endl;
+        cout << n << "*1=" << n * 2 << endl;
+        cout << n << "*1=" << n * 3 << endl;
+        cout << n << "*1=" << n * 4 << endl;
+        cout << n << "*1=" << n * 5 << endl;
+        cout << n << "*1=" << n * 6 << endl;
+        cout << n << "*1=" << n * 7 << endl;
+        cout << n << "*1=" << n * 8 << endl;
+        cout << n << "*1=" << n * 9 << endl;
+        cout << n << "*1=" << n * 10 << endl;
+        return 0;
+    }
+
