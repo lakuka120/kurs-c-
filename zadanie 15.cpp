a13bfbfd369ad7b28f7e2478ae794a04a8cff76a
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-25 14:31:49 +0200

    Implement suma function to add two integers
    Fix function declaration and call for suma.

---
+++ zadanie 15.cpp
@@ -0,0 +1,17 @@
+#include<iostream>      
+#include<cstring>
+#include<cmath>
+#include<cctype>
+
+using namespace std;
+
+void suma ()
+int dodaj(int a, intb) {
+	int suma= a + b;
+	return suma;
+}
+intmain() { 
+	cout<< suma (5, 9) << endl;
+	cout << suma(13, 51);
+	return 0;
+}
