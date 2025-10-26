1094a9e71b6b24aa3be4f4cbeb180c02777b24f1
lakuka120 <Wiktor.slysz@o2.pl>
2025-10-26 09:15:58 +0100

    Implement program to input and display 5 numbers

---
+++ tabela5liczb.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+int main () {
+	int tablica [5];
+
+cout << "podaj 5 liczb";
+for (int i = 0; i < 5; i++)
+{
+	cin >> tablica[i];
+}
+cout << "liczby";
+for (int i = 0; i < 5; i++)
+{
+	cout << tablica[1] << " ";
+
+}
+cout << endl;
+return 0;
+]
